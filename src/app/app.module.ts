70a5710bad93a1722408cf6616e340f256cd142b
Juanpa <jupmorenor@gmail.com>
2020-10-16 18:34:36 -0500

    Angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,13 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,13 +29,17 @@ import { DishService } from './services/dish.service';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     FlexLayoutModule,
+    AppRoutingModule,
+    MatToolbarModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
