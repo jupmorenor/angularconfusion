3cf84cd03fd8d8ebc4e71ccb4043c7082faaf381
Juanpa <jupmorenor@gmail.com>
2021-02-07 12:36:43 -0500

    Unit testing of the Menu component

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,9 @@
 import { HighlightDirective } from './highlight.directive';
+import { ElementRef, HostListener } from '@angular/core';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    const directive = new HighlightDirective(null, null);
     expect(directive).toBeTruthy();
   });
 });
